6f26bcadf52ddc37a096f3ab5dd3d3c7a995ca94
rawfid <rawfid.shuvo@outlook.com>
2025-12-25 23:08:55 +0100

    Added Game Inspiration

--- src/App.jsx
+++ src/App.jsx
@@ -1709,7 +1709,7 @@ export default function ConspiracyGame() {
           </button>
         </div>
         <div className="absolute bottom-4 text-slate-600 text-xs text-center">
-          Inspired by Sea, Salt & Paper. <br /> A tribute game.
+          Inspired by Coup. <br /> A tribute game.
         </div>
       </div>
     );
